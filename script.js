c0466ff440983f77332f4ecb0f23f124d2ad9ff4
LenaBultmann <lena.bultmann@gmx.de>
2023-11-30 16:33:37 +0100

    tweaking js and responsivenes

--- script.js
+++ script.js
@@ -1,19 +1,27 @@
+const linkClick = document.querySelectorAll(".funcLink");
+const recipes = document.querySelector("#iframe");
 
+linkClick.forEach((element) => element.addEventListener("click", changeIframe));
 
+function changeIframe(e) {
+  const arr = e.target.classList;
+  console.log(arr);
 
-// document.querySelector(".mousaka").addEventListener("click",changeIframe);
-
-// function changeIframe() {
-//     document.getElementById("recipes").src = "./recipes/greek/mousaka.html"; 
-// }
-
-
-
-const recipes = document.querySelector('#iframe')
-
-console.log(recipes);
-
-const handleClick = (recipe) => {
-    recipes.src = `./recipes/${recipe}.html`
+  if (Object.values(arr).includes("greek")) {
+    recipes.src = `./recipes/greek/${e.target.id}.html`;
+  } else if (Object.values(arr).includes("german")) {
+    recipes.src = `./recipes/german/${e.target.id}.html`;
+  } else {
+    recipes.src = `./home.html`;
+  }
 }
 
+// Enriques Solution
+
+// const recipes = document.querySelector('#iframe')
+
+// console.log(recipes);
+
+// const handleClick = (recipe) => {
+//     recipes.src = `./recipes/${recipe}.html`
+// }
